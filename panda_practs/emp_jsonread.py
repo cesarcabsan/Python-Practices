3faa09b7baabc17f2afe18bbda2e529a51d127a4
Cesar Cabrera <cesarcabs17@gmail.com>
2025-12-18 13:52:14 -0600

    Update: this file is now moved to json_files folder on Documents

--- panda_practs/emp_jsonread.py
+++ panda_practs/emp_jsonread.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_json("C:\\Users\\cesar\\OneDrive\\Documents\\employee_performance.json")
+df = pd.read_json("C:\\Users\\cesar\\OneDrive\\Documents\\json_files\\employee_performance.json")
 print(df)
 
 # json files can be printed in a variety of orientations
@@ -10,4 +10,3 @@ print("\nIndex orientation:\n", df.to_json(orient='index')) # orient='index': sh
 print("\nValues orientation:\n", df.to_json(orient='values')) # orient='values': outputs a list of lists, where each inner list represents a row of values without any column names or index information.
 print("\nRecords orientation:\n", df.to_json(orient='records')) # orient='records': Represents each row of the DataFrame as a separate JSON object within a list.
 print("\nTable orientation:\n", df.to_json(orient='table')) # orient='table': Data is formmated as a table, including a 'schema' that describes the columns and their data types, along with the 'data' itself
- 
